2dcdb5eb5a84026ef96e7c4555d4bfecfac6261c
twojaylee <twojaylee@gmail.com>
2023-11-24 22:30:36 +0900

    11 commit

--- tisdad.js
+++ tisdad.js
@@ -1,10 +1,7 @@
 $(document).ready(
     function(){
-        $("#s_permalink_article_rep:nth-child(2)")
-        .after("https://github.com/twojaylee/tis-con/blob/main/index.html #tdad");
-        $("#body")
-        .after("https://github.com/twojaylee/tis-con/blob/main/index.html #allpo");
-        $("#body")
-        .after("https://github.com/twojaylee/tis-con/blob/main/index.html #blkaad");
+        $("#s_permalink_article_rep:nth-child(2)").after("https://github.com/twojaylee/tis-con/blob/main/index.html #tdad");
+        $("#body").after("https://github.com/twojaylee/tis-con/blob/main/index.html #allpo");
+        $("#body").after("https://github.com/twojaylee/tis-con/blob/main/index.html #blkaad");
     }
 );
